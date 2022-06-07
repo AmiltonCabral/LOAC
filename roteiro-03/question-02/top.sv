46bc507bb2be565464fd20b15881749ba6f8d936
AmiltonCristian <amilton.cabral@ccc.ufcg.edu.br>
2022-06-07 14:07:35 -0300

    submited code

---
+++ roteiro-03/question-02/top.sv
@@ -0,0 +1,60 @@
+// Amilton Cristian Pereira Cabral - Turma 1
+// Roteiro 03 - Questao 02
+
+parameter divide_by=100000000;  // divisor do clock de referência
+// A frequencia do clock de referencia é 50 MHz.
+// A frequencia de clk_2 será de  50 MHz / divide_by
+
+parameter NBITS_INSTR = 32;
+parameter NBITS_TOP = 8, NREGS_TOP = 32, NBITS_LCD = 64;
+module top(input  logic clk_2,
+           input  logic [NBITS_TOP-1:0] SWI,
+           output logic [NBITS_TOP-1:0] LED,
+           output logic [NBITS_TOP-1:0] SEG,
+           output logic [NBITS_LCD-1:0] lcd_a, lcd_b,
+           output logic [NBITS_INSTR-1:0] lcd_instruction,
+           output logic [NBITS_TOP-1:0] lcd_registrador [0:NREGS_TOP-1],
+           output logic [NBITS_TOP-1:0] lcd_pc, lcd_SrcA, lcd_SrcB,
+             lcd_ALUResult, lcd_Result, lcd_WriteData, lcd_ReadData,
+           output logic lcd_MemWrite, lcd_Branch, lcd_MemtoReg, lcd_RegWrite);
+
+  always_comb begin
+    lcd_WriteData <= SWI;
+    lcd_pc <= 'h12;
+    lcd_instruction <= 'h34567890;
+    lcd_SrcA <= 'hab;
+    lcd_SrcB <= 'hcd;
+    lcd_ALUResult <= 'hef;
+    lcd_Result <= 'h11;
+    lcd_ReadData <= 'h33;
+    lcd_MemWrite <= SWI[0];
+    lcd_Branch <= SWI[1];
+    lcd_MemtoReg <= SWI[2];
+    lcd_RegWrite <= SWI[3];
+    for(int i=0; i<NREGS_TOP; i++)
+       if(i != NREGS_TOP/2-1) lcd_registrador[i] <= i+i*16;
+       else                   lcd_registrador[i] <= ~SWI;
+    lcd_a <= {56'h1234567890ABCD, SWI};
+    lcd_b <= {SWI, 56'hFEDCBA09876543};
+  end
+
+  // Criando as variaveis A e B, e a variavel de selecao
+  logic [2:0] a;
+  logic [2:0] b;
+  logic [1:0] sel;
+
+  always_comb begin
+
+    //atribuindo os valores as variavies
+    a <= SWI[4:3];
+    b <= SWI[6:5];
+    sel <= SWI[7];
+
+    if (sel == 0)  // Se o SWI[7] estiver desligado, exibe A
+      LED[7:6] <= a;
+    else           // Se o SWI[7] estiver ligado, exibe B
+      LED[7:6] <= b;
+
+  end
+
+endmodule
